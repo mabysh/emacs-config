0f1d47425d1db53f297979cd76177d4eea70935b
Max Byshnev <max.byshnev@zoho.com>
2017-08-08 23:28:32 +0300

    basic emacs configuration

---
+++ init.el
@@ -0,0 +1,11 @@
+(add-to-list 'load-path (expand-file-name "conf" user-emacs-directory))
+
+(require 'init-elpa)
+(require 'init-ui)
+(require 'init-editing)
+(require 'init-navigation)
+(require 'init-misc)
+(require 'init-company-mode)
+
+(provide 'init)
+
