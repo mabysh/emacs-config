0e4f13941456e1ec21dabd93940a6c645b83feac
Max Byshnev <max.byshnev@zoho.com>
2017-08-10 21:49:09 +0300

    dired

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "conf" user-emacs-directory))
 
 (require 'init-elpa)
@@ -8,6 +15,8 @@
 (require 'init-company-mode)
 (require 'init-rust)
 (require 'init-session)
+(require 'init-dired)
+
 
 (provide 'init)
 
